8cb88997e1736d3a883ca2c7912332fee7ca0ef4
sp010412 <smajwede7@gmail.com>
2021-03-29 12:24:43 +0200

    courage

---
+++ countRegNumber.test.js
@@ -0,0 +1,8 @@
+describe('countRegNumber', function () {
+    it('should return the number of registration numbers in the string', function () {
+        assert.equal(countRegNumber('CA 182736,CY 523519,CJ 812328'), 3);
+
+    });
+
+
+});
---
+++ greet.test.js
@@ -0,0 +1,7 @@
+describe('Greet' , function(){
+    it('should show you a greet message and name' , function(){
+        assert.equal(greet('Cara'),'Hello, Cara')
+
+    });
+
+});
